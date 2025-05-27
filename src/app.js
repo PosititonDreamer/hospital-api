caf4b249db852aa5fb888f281a14993076b378fc
PositionDreamer <nulva1@yandex.ru>
2025-05-27 19:13:22 +0500

    add cors, change routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const pool = require('./config/db');
-
+const router = require('./routes');
+const cors = require('cors')
 const app = express();
 
-pool.query('SELECT NOW()', (err, res) => {
-    if(err) {
-        console.error('Error connecting to the database', err.stack);
-    } else {
-        console.log('Connected to the database:', res.rows);
-    }
-});
-
+app.use(cors());
+app.use('/api/v1/', router);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const router = new Router()
 const usersController = require('../controllers/userController')
 
-router.post('/api/v1/auth/register', usersController.register)
-router.post('/api/v1/auth/login', usersController.login)
-router.get('/api/v1/auth/profile', usersController.profile)
+router.post('auth/register', usersController.register)
+router.post('auth/login', usersController.login)
+router.get('auth/profile', usersController.profile)
 
 module.exports = router
